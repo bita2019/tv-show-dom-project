52c24aca5b1e2888a6c0878dae7ade619a293dbe
bita2019 <58438858+bita2019@users.noreply.github.com>
2022-03-02 23:41:10 +0000

    tv show 100-350

--- script.js
+++ script.js
@@ -17,10 +17,44 @@ const searchBox = document.getElementById("search-box");
 const searchCount = document.getElementById("search-count");
 
 //You can edit ALL of the code here
+function getLiveEpisodes() {
+  var newVar = fetch("https://api.tvmaze.com/shows/82/episodes")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("This is data", data);
+      // data.forEach((e) => {
+      //   const episode = document.createElement("div");
+      //   const heading = document.createElement("h3");
+      //   const episodeImage = document.createElement("img");
+      //   const summary = document.createElement("p");
+
+      //   heading.innerText = `${e.name} - ${formatSeriesAndEpisode(
+      //     e.season,
+      //     e.number
+      //   )}`;
+
+      //   summary.innerHTML = e.summary;
+      //   episodeImage.src = e.image.medium;
+      //   episode.className = "episode";
+
+      //   episode.appendChild(heading);
+      //   episode.appendChild(episodeImage);
+      //   episode.appendChild(summary);
+      //   episodeContainer.appendChild(episode);
+      // });
+    });
+}
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  // const allEpisodes = getLiveEpisodes();
+
   searchBox.addEventListener("keyup", onSearchKeyUp);
+  fetch("https://api.tvmaze.com/shows/82/episodes")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("This is data", data);
+      makePageForEpisodes(data);
+    });
 }
 
 function makePageForEpisodes(episodeList) {
@@ -58,7 +92,7 @@ function makePageForEpisodes(episodeList) {
 
 function onSearchKeyUp(event) {
   const searchTerm = event.target.value.toLowerCase();
-  const allEpisodes = getAllEpisodes();
+  // const allEpisodes = getLiveEpisodes();
 
   const filteredEpisodes = allEpisodes.filter((e) => {
     const episodeName = e.name.toLowerCase();
